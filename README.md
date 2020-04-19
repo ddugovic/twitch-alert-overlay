4d0cd6131af14205644098ba3493d91143005cc0
JoeKombo <63080329+joekombo@users.noreply.github.com>
2020-04-19 03:21:53 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,77 @@
+![screenshot of stream alert overlay](screenshot.png)
+
+# Stream Alert Overlay
+
+A simple follower alert overlay for OBS that displays a list of the latest followers approximately every 60 seconds.
+
+_Note: This project uses the `docs/` folder for web related files: `.html`, `.css`, `.js` etc. I do this because the project is hosted on [github pages](https://pages.github.com/)._
+
+## Parameters
+
+### `login`
+
+Default `joekombo`. Enter the login name for a Twitch user.
+
+### `volume`
+
+Default `25`. Percentage from `0` to `100`.
+
+### `sound`
+
+Default `guitar`. Must match a filename found in `docs/sounds`
+
+### `gif`
+
+Default `guitarkitty`. Positioned in the bottom left of screen. Must match a filename found in `docs/images`.
+
+### `clientid`
+
+Required. [Create an application at dev.twitch.tv](https://dev.twitch.tv) and use that client id for `clientid`.
+
+## Example
+
+`https://joekombo.github.io/stream-alert-overlay/?login=joekombo&clientid=[your client id]`
+
+## Application Settings
+
+I use the following application settings at [dev.twitch.tv](https://dev.twitch.tv) to obtain my Client ID. The Client ID can be found near the bottom.
+
+![screenshot of dev.twitch.tv settings](twitch-app-settings.png)
+
+## Run locally
+
+Clone the project using [git](https://git-scm.com). And then change the directory to the project folder.
+
+```
+git clone https://github.com/joekombo/stream-alert-overlay
+cd stream-alert-overlay
+```
+
+Install the depedencies using [npm](https://www.npmjs.com/) and use `npm start` to serve the files.
+
+```
+npm install
+npm start
+```
+
+Go to web browser and view the page.
+
+Example URL:
+
+`http://localhost:5000/?login=joekombo&clientid=[your client id]`
+
+_Note: Some browsers require permission to play sound. Click the tab and select allow audio/video._
+
+## OBS
+
+Create a `Browser` source, input the URL, and then set the width to 1920 and height to 1080.
+
+## Contributions
+
+To contribute changes, please [fork the repository](https://help.github.com/en/github/getting-started-with-github/fork-a-repo), make the changes, and then [create a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request)! Thank you!.
+
+_Note: [This project uses Prettier](https://prettier.io/) to format the code automatically when new code is commited. No additional steps are needed for this to work. More details about how this is setup is found [here](https://create-react-app.dev/docs/setting-up-your-editor/#formatting-code-automatically)._
+
+## Issues
+
+Something not working, or you're getting errors? Then please [submit an issue](https://help.github.com/en/github/managing-your-work-on-github/creating-an-issue).
---
+++ docs/main.js
@@ -0,0 +1,142 @@
+async function fetchUserId(login, clientid) {
+  try {
+    const response = await fetch(
+      `https://api.twitch.tv/helix/users?login=${login}`,
+      {
+        headers: { "Client-ID": clientid },
+      }
+    );
+    const json = await response.json();
+    return json.data[0].id;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function fetchFollowerIds(userId, clientId) {
+  try {
+    const response = await fetch(
+      `https://api.twitch.tv/helix/users/follows?to_id=${userId}&first=100`,
+      {
+        headers: { "Client-ID": clientId },
+      }
+    );
+    const json = await response.json();
+    return json.data.map((x) => x.from_id);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function fetchFollowerDetails(followerIds, clientid) {
+  try {
+    const followersString = followerIds.map((id) => `id=${id}`).join("&");
+    const response = await fetch(
+      `https://api.twitch.tv/helix/users?${followersString}`,
+      {
+        headers: { "Client-ID": clientid },
+      }
+    );
+    const json = await response.json();
+    return json.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function configureSound(name, volume) {
+  const sound = document.getElementById("alert-sound");
+
+  sound.src = `sounds/${name}.wav`;
+  sound.volume = volume / 100;
+
+  return sound;
+}
+
+function buildAlert(detail) {
+  return `
+  <div class="alert">
+    <img
+      class="profile-picture"
+      src="${detail.profile_image_url}"
+    />
+    <div class="name-group">
+      <div class="followed-by">&#x1F90D; Followed By</div>
+      <div class="display-name">${detail.display_name}</div>
+    </div>
+  </div>`;
+}
+
+async function startOverlay() {
+  try {
+    const params = new URLSearchParams(location.search);
+    const login = params.get("login") || "joekombo";
+    const sound = params.get("sound") || "guitar";
+    const volume = Number(params.get("volume") || "50");
+    const clientId = params.get("clientid");
+    const gif = params.get("gif") || "guitarkitty";
+    const soundClip = configureSound(sound, volume);
+
+    const userId = await fetchUserId(login, clientId);
+    const initialFollowerIds = await fetchFollowerIds(userId, clientId);
+
+    if (!clientId) {
+      document.body.innerHTML = `
+        <div class='instructions'>
+          <h1>MISSING CLIENT ID</h1>
+          <p>Go to <a href="https://github.com/joekombo/stream-alert-overlay">
+          https://github.com/joekombo/stream-alert-overlay</a> for detailed instructions.</p>
+        </div>
+      `;
+      return;
+    }
+
+    let knownFollowersIds = initialFollowerIds;
+
+    setInterval(async () => {
+      const followerIds = await fetchFollowerIds(userId, clientId);
+      let newFollowersIds = [];
+
+      followerIds.forEach((id) => {
+        if (!knownFollowersIds.includes(id)) {
+          knownFollowersIds.push(id);
+          newFollowersIds.push(id);
+        }
+      });
+
+      if (newFollowersIds.length) {
+        const followerDetails = await fetchFollowerDetails(
+          newFollowersIds,
+          clientId
+        );
+
+        let remainingDetails = followerDetails;
+
+        let alertLoopInterval = setInterval(() => {
+          let first15Alerts = remainingDetails
+            .splice(0, 15)
+            .map((detail) => buildAlert(detail));
+
+          soundClip.play();
+
+          document.getElementById("alerts").innerHTML = `
+            <div id='alerts-frame'>
+              ${first15Alerts.join("")}
+            </div>
+            <img id='gif' src='images/${gif}.gif' />
+            `;
+
+          if (remainingDetails.length <= 0) {
+            clearInterval(alertLoopInterval);
+          }
+
+          console.log(`Remaining alerts: ${remainingDetails.length}`);
+        }, 5000);
+      }
+    }, 15000);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startOverlay();
